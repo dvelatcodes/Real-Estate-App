e9c4cb087238f6c4e1c07eb7428778749c9142e9
Love Olubummo <olubummolove3@gmail.com>
2023-04-14 09:02:10 +0100

    fix: created a dotenv file and stored api key

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = "https/bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": "f32063f3camshd5d011e654c416ap10d6e8jsnebdc02005f43",
+      "X-RapidAPI-Key": process.env.RapidApiKey,
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
