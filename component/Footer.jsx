0112323d58a37c48807079bd563027c364b4bec7
Love Olubummo <olubummolove3@gmail.com>
2023-04-15 18:30:32 +0100

    feature: created a Footer and Navbar component

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Box } from "@chakra-ui/layout";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+const Footer = () => (
+  <Box
+    textAlign="center"
+    p="5"
+    color="gray.600"
+    borderTop="1px"
+    borderColor="gray.100"
+  >
+    © 2023 Realtor, Inc.
+  </Box>
+);
 
 export default Footer;
